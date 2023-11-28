77cb17a181b41ace2d10e8f8c827f0555c03978c
Oleh Bilyi <oleh.bilyj14@gmail.com>
2023-11-28 13:07:48 +0200

    Method updateUser() in DynamoDBServiceImpl was implemented

--- DynamoDBApp/src/main/java/layer/service/DynamoDBServiceImpl.java
+++ DynamoDBApp/src/main/java/layer/service/DynamoDBServiceImpl.java
@@ -80,12 +80,17 @@ public class DynamoDBServiceImpl implements DynamoDBService {
     @Override
     public String updateUser(Map<String, String> pathParameters, String inputBody) {
 
-        //TODO Implement a record update by Id
-        /*
-        Add code that accepts parameters, finds required record by Id,
-        and update it in the table record according to provided parameters.
-        Returns a text message as operation result.
-        */
+        User user = new User();
+        user.setEmail(pathParameters.get(TABLE_PARTITION_KEY));
+
+        User existingUser = dynamoDBMapper.load(User.class, user.getEmail());
+        if (existingUser != null) {
+            User inputuser = new Gson().fromJson(inputBody, User.class);
+            updateUsersNotNullAttributes(existingUser,inputuser);
+            return getJsonResponse("User was updated");
+        } else {
+            return getJsonResponse("User not found");
+        }
     }
 
     @Override
